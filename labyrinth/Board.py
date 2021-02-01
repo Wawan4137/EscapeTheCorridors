752a25bf2c36ca9abfe060af5fbc36025f292711
erwan.gaychanteloup <erwan.gaychanteloup@gmail.com>
2021-02-01 10:08:29 +0100

    ajout de la base

--- labyrinth/Board.py
+++ labyrinth/Board.py
@@ -1,2 +1,37 @@
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ListProperty, BooleanProperty
+from .tile import Tile
+from kivy.metrics import dp
+from kivy.parser import parse_color
+from random import randint
+from kivy.clock import Clock
+
+COLORS = ["#804515", "#D49A6A", "#FFD1AA"]
+
 class Board(Widget):
+
+    nrows = NumericProperty(8)
+    ncols = NumericProperty(8)
+    tsize = NumericProperty(0)
+    tmargin = NumericProperty(dp(10))
+    tx = NumericProperty(0)
+    ty = NumericProperty(0)
+    margin = NumericProperty(dp(1))
+    colors = ListProperty([parse_color(c) for c in COLORS])
+    mode_all = BooleanProperty(False)
+    torus = BooleanProperty(False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.matrix = None
+        self.create()
     
+    def create(self):
+        if self.matrix:
+            for ligne in self.matrix:
+                for tile in ligne:
+                    self.remove_widget(tile)
+        self.matrix = [
+            [Tile(i, j, self) for j in range(self.ncols)]
+            for i in range(self.nrows)
+        ]
---
+++ labyrinth/tile.py
@@ -0,0 +1,19 @@
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ObjectProperty
+from kivy.animation import Animation
+
+class Tile(Widget):
+
+    row = NumericProperty(0)
+    col = NumericProperty(0)
+    board = ObjectProperty(None)
+    state = NumericProperty(0)
+    rgba = ObjectProperty((1,1,1,1))
+
+    def __init__(self, ligne, colonne, board):
+        self.row = ligne
+        self.col = colonne
+        self.board = board
+        super().__init__()
+        self.rgba = self.board.colors[0]
+        self.board.add_widget(self)
