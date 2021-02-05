7c59edd35a17c07894f6865f334e60821c1af98a
Lucas <lucas.desnoue@free.fr>
2021-02-05 16:32:33 +0100

    Detection des pieces + Condition de victoire

--- labyrinth/board.py
+++ labyrinth/board.py
@@ -1,5 +1,5 @@
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ListProperty, BooleanProperty
+from kivy.properties import NumericProperty, ListProperty, BooleanProperty, ObjectProperty
 from kivy.metrics import dp
 from kivy.parser import parse_color
 
@@ -26,12 +26,16 @@ class Board(Widget):
     tmargin = NumericProperty(dp(1))
     colors = ListProperty([parse_color(c) for c in COLORS])
 
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.matrix = None
+        self.player = None
+        self.totalCoins = 0
+        self.nbCoins = 0
         self.display()
         self.getLevel()
-        print(self.matrix[1][0].prefab)
+        self.findPlayer()
 
     def display(self):
         level = self.getLevel()
@@ -44,9 +48,16 @@ class Board(Widget):
             for i in range(self.nrows)
         ]
 
+    def findPlayer(self):
+        if self.matrix:
+            for r in self.matrix:
+                for t in r:
+                    if isinstance(t.prefab, Player):
+                        self.player = t
+                        return
 
     def getLevel(self):
-        level = open('labyrinth/levels/level3.txt','r')
+        level = open('labyrinth/levels/levelMove.txt','r')
         matrixPrefabs = [
             [None for j in range(self.ncols)]
             for i in range(self.nrows)
@@ -59,7 +70,6 @@ class Board(Widget):
                     matrixPrefabs[j][i] = self.getRealPrefab(y)
                 i += 1
             j+=1
-        print (matrixPrefabs)
         return matrixPrefabs
                   
 
@@ -71,9 +81,57 @@ class Board(Widget):
         if char == "e":
             return Empty()
         if char == "c":
+            self.totalCoins += 1
             return Coin()
         if char == "p":
             return Player()
         if char == "s":
             return Floor()
+
+    #Event click on move button
+    #Action = "up","down","left","right"
+    def movePlayer(self, action):
+        iPlayer = self.player.row
+        jPlayer = self.player.col
+        actionDone = False
+        if action == "up":
+            if not (isinstance(self.matrix[iPlayer+1][jPlayer].prefab, Wall) or isinstance(self.matrix[iPlayer+1][jPlayer].prefab, Empty)):
+                self.player.row = iPlayer+1
+                self.checkCoin()
+                self.matrix[self.player.row][jPlayer] = self.player
+                actionDone = True
+        if action == "down":
+            if not (isinstance(self.matrix[iPlayer-1][jPlayer].prefab, Wall) or isinstance(self.matrix[iPlayer-1][jPlayer].prefab, Empty)):
+                self.player.row = iPlayer-1
+                self.checkCoin()
+                self.matrix[self.player.row][jPlayer] = self.player
+                actionDone = True
+        if action == "left":
+            if not (isinstance(self.matrix[iPlayer][jPlayer-1].prefab, Wall) or isinstance(self.matrix[iPlayer][jPlayer-1].prefab, Empty)):
+                self.player.col = jPlayer-1
+                self.checkCoin()
+                self.matrix[iPlayer][self.player.col] = self.player
+                actionDone = True
+        if action == "right":
+            if not (isinstance(self.matrix[iPlayer][jPlayer+1].prefab, Wall) or isinstance(self.matrix[iPlayer][jPlayer+1].prefab, Empty)):
+                self.player.col = jPlayer+1
+                self.checkCoin()
+                self.matrix[iPlayer][self.player.col] = self.player
+                actionDone = True       
+        if actionDone:         
+            tmpTile = Tile(iPlayer,jPlayer, self, Empty())
+            self.matrix[iPlayer][jPlayer] = tmpTile
+        
+    
+    def checkCoin(self):
+        if isinstance(self.matrix[self.player.row][self.player.col].prefab, Coin):
+            self.nbCoins += 1
+            print(self.nbCoins)
+        if self.nbCoins*2 >= self.totalCoins:
+            if isinstance(self.matrix[self.player.row][self.player.col].prefab, Finish):
+                print("gg")
+
+            
+ 
+
             
